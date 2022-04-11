90e9441b1828abd1b0ec59339ddf7359d78e25a9
taddeal <taddealmoges@gmail.com>
2022-04-11 11:45:12 -0700

    jhg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - Entrey point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+ptintf("size of a char: %d bytes(s)\n", sizeof(char));
+ptintf("size of an int: %d bytes(s)\n", sizeof(int));
+ptintf("size of a long int: %d bytes(s)\n", sizeof(long int));
+ptintf("size of a long long int: %d bytes(s)\n", sizeof(lon long int));
+ptintf("size of a float: %d bytes(s)\n", sizeof(float));
+return (0);
+}
