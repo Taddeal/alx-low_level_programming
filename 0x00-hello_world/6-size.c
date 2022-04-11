c52f0e6888e4ec7bac621995e0a66b2558c9afb7
taddeal <taddealmoges@gmail.com>
2022-04-11 11:59:51 -0700

    ggg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - Entrey point
+*main - Entry point
 *
 *Return: Always 0 (Success)
 */
@@ -9,7 +9,6 @@ int main(void)
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
-printf("size of a long long int: %d byte(s)\n", sizeof(lon long int));
 printf("size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
