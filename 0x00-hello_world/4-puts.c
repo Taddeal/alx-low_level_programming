ddb89354fa7e0845c7889f2dcf144580e840a2c0
taddeal <taddealmoges@gmail.com>
2022-04-11 10:46:11 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(void)
+/* main function prints the text below*/
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return(0);
 }
